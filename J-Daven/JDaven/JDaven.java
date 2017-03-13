87e03200f74a615e63c7375ff359d5f299af1457
GreatJack <wyre@gmail.com>
2017-03-12 20:04:44 -0400

    Added a partial implementation to the minyan class

---
+++ J-Daven/JDaven/JDaven.java
@@ -0,0 +1,21 @@
+/**
+ * Created by yaakov on 3/12/17.
+ */
+public class JDaven {
+    /**
+     * This constructor takes a string containing the name of the shul and returns a list of shuls with their minyanim
+     */
+    public JDaven(String shulName) {
+
+
+    }
+
+    /**
+     * This constructor takes a zipcode as a integer and returns a list of shuls near that zipcode and their minyanim
+     */
+    public JDaven(int zipcode) {
+
+
+    }
+
+}
---
+++ J-Daven/JDaven/Minyan.java
@@ -0,0 +1,55 @@
+/**
+ * Created by yaakov on 3/12/17.
+ * This class ecapsulates a Minyan and its davening times
+ */
+public class Minyan {
+
+    private int mId;
+    private String mName;
+    private String mAddress1;
+    private String mAddress2;
+    private String mCity;
+    private String mState;
+    private String mCountry;
+    private String mPhone1;
+    private String mPhone2;
+    private String mEmail;
+    private String mWebsite;
+    private String mRabbi;
+    private String mShachrisSunday1;
+    private String mShachrisSunday2;
+    private String mShachrisSunday3;
+    private String mShachrisSunday4;
+    private String mShachrisSunday5;
+    private String mShachrisSundayText;
+    private String mShachrisMonThur1;
+    private String mShachrisMonThur2;
+    private String mShachrisMonThur3;
+    private String mShachrisMonThur4;
+    private String mShachrisMonThur5;
+    private String mShachrisMonThurText;
+    private String mShachrisTueWedFri1;
+    private String mShachrisTueWedFri2;
+    private String mShachrisTueWedFri3;
+    private String mShachrisTueWedFri4;
+    private String mShachrisTueWedFri5;
+    private String mShachrisTueWedFriText;
+    private String mShachrisRoshChodesh1;
+    private String mShachrisRoshChodesh2;
+    private String mShachrisRoshChodesh3;
+    private String mShachrisRoshChodesh4;
+    private String mShachrisRoshChodesh5;
+    private String mShachrisRoshChodeshText;
+    private String mMincha1;
+    private String mMincha2;
+    private String mMinchaText;
+    private String mMaariv1;
+    private String mMaariv2;
+    private String mMaarivText;
+    private String mFridayMincha;
+    private String mFridayMinchaText;
+    private String mShabbosShachris1;
+    private String mShabbosShachris2;
+    private String mShabbosShachris3;
+    private String mShabbosShachrisText;
+}
